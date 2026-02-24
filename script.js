df28c1ecc364c28d142090d2d112c25ed4055b1e
Hassaan Nadeem <222315448+HassaanN08@users.noreply.github.com>
2026-02-24 23:42:01 +0500

    fix code logic and add keyboard functionality to it

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-const input = document.querySelector('input');
+const input = document.querySelector('.input');
 const operatorBtns = document.querySelectorAll('.operator');
 const numberBtnWrapper = document.querySelector('.numbers');
+const decimalBtn = document.querySelector('.decimal')
 const equalButton = document.querySelector('.equal');
 const answer = document.querySelector('.answer');
 const clearBtn = document.querySelector('.clear');
 const delBtn = document.querySelector('.backspace');
 
-let firstNumber;
-let operator;
-let lastNumber;
-let operatorClicked = false;
-let equalSignClicked = false;
+let decimalClicked = false;
 
 clearBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    firstNumber = null;
-    lastNumber = null;
-    operatorClicked = false;
-    equalSignClicked = false;
     answer.textContent = '0';
-    input.value = null;
+    input.textContent = null;
+    decimalClicked = false;
 })
 
 delBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    input.value = input.value
-        .split('')
-        .slice(0, -1)
-        .join('');
+    const currentVal = Array.from(input.textContent);
     
-    console.log(input.value);
+    if (currentVal.at(-2) == '+' || currentVal.at(-2) == '-' || currentVal.at(-2) == '*' || currentVal.at(-2) == '/') {
+        input.textContent = currentVal
+            .slice(0, -3)
+            .join('');
+        
+        if (input.textContent.split(' ').at(-1).split('').includes('.')) {
+            decimalClicked = true;
+        } else {
+            decimalClicked = false;
+        }
+    } else {
+        if (currentVal.at(-1) == '.') {
+            decimalClicked = false;
+        }
+        input.textContent = currentVal
+            .slice(0, -1)
+            .join('');
+    }
 })
 
 for (let i = 9; i >=0; i--) {
@@ -68,46 +76,88 @@ const operate = (ope, aStr, bStr) => {
     } else if (ope == '*') {
         return multiply(a, b);
     } else if (ope == '/') {
-        return divide(a, b);
+        if (b == 0) {
+            return 'Yeah no.'
+        } else {
+            return divide(a, b);
+        }
     }
 }
 
 const numberClickEventHandler = (e) => {
     e.preventDefault();
-    if (!operatorClicked) {
-        input.value += e.target.value;
-        firstNumber = input.value;
-        console.log(firstNumber);
-    } else {
-        input.value += e.target.value;
-        const lastNumberArr = input.value.split(' ');
-        lastNumber = lastNumberArr[lastNumberArr.length - 1];
-        console.log(lastNumber);
-        firstNumber = String(operate(operator, firstNumber, lastNumber));
-        equalButton.addEventListener('click', equalToClickEventHandler);
+    if (input.textContent == 'NaN' || input.textContent == 'Yeah no.') {
+        input.textContent = e.target.value;
     }
+
+    input.textContent += e.target.value;
+}
+
+const decimalClickEventHandler = (e) => {
+    e.preventDefault();
+    if (input.textContent == 'NaN' || input.textContent == 'Yeah no.') {
+        input.textContent = e.target.value;
+    } else if (!decimalClicked) {
+        input.textContent += e.target.value;
+    }
+    
+    decimalClicked = true;
 }
 
 const operatorClickEventHandler = (e) => {
     e.preventDefault();
-    if (firstNumber) {
-        operatorClicked = true;
+    let currentVal = input.textContent.split(' ');
+
+    if (input.textContent == 'NaN' || input.textContent == 'Yeah no.') {
+        input.textContent = ` ${e.target.value} `;
+    } else if (['+', '-', '*', '/'].includes(currentVal.at(-2)) && !currentVal.at(-1)) {
+        currentVal.splice(-2, 1, `${e.target.value}`);
+        input.textContent = currentVal.join(' ');
+    } else {
+        input.textContent += ` ${e.target.value} `;
     }
-    input.value += ` ${e.target.value} `;
-    operator = e.target.value;
-    console.log(operator);
+    
+    decimalClicked = false;
 }
 
 const equalToClickEventHandler = (e) => {
-    e.preventDefault;
-    let value = firstNumber;
-    answer.textContent = String(firstNumber);
-    console.log(operate(operator, firstNumber, lastNumber));
-    console.log(firstNumber);
-    console.log(operator);
-    console.log(lastNumber);
-    input.value = value;
-    equalButton.removeEventListener('click', equalToClickEventHandler);
+    e.preventDefault();
+
+    const inputArr = input.textContent
+        .split(' ')
+        .map(item => (item || '0'));
+    let prevNum;
+    let nextNum;
+
+    if (!['+', '-', '*', '/'].includes(inputArr[0])) {
+        prevNum = inputArr[0];
+    } else {
+        prevNum = '0';
+    }
+
+    if (inputArr.length > 1) {
+        for (let i = 0; i <= inputArr.length - 1; i++) {
+            if (['+', '-', '*', '/'].includes(inputArr[i])) {
+                if (i == inputArr.length - 1) {
+                    nextNum = '0';
+                } else {
+                    nextNum = inputArr[i + 1];
+                }
+                let newNum = operate(inputArr[i], prevNum, nextNum);
+                prevNum = newNum;
+                console.log(prevNum);
+            }
+        }
+    }
+
+    input.textContent = prevNum;
+    answer.textContent = prevNum;
+
+    if (input.textContent.split('').includes('.')) {
+            decimalClicked = true;
+        } else {
+            decimalClicked = false;
+        }
 }
 
 numberBtns.forEach((numberBtn) => {
@@ -116,4 +166,116 @@ numberBtns.forEach((numberBtn) => {
 
 operatorBtns.forEach((operatorBtn) => {
     operatorBtn.addEventListener('click', operatorClickEventHandler);
+})
+
+equalButton.addEventListener('click', equalToClickEventHandler);
+
+decimalBtn.addEventListener('click', decimalClickEventHandler);
+
+const allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Enter', 'Backspace', '.', 'Escape', '*', '+', '/', '-'];
+const numberKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+document.addEventListener('keydown', (e) => {
+
+    if(allowedKeys.includes(e.key)) {
+        e.preventDefault();
+        if (numberKeys.includes(e.key)) {
+            if (input.textContent == 'NaN' || input.textContent == 'Yeah no.') {
+                input.textContent = e.key;
+            }
+            input.textContent += e.key;
+        }
+
+        if (e.key == '.') {
+            if (input.textContent == 'NaN' || input.textContent == 'Yeah no.') {
+                input.textContent = e.key;
+            } else if (!decimalClicked) {
+                input.textContent += e.key;
+            }
+            
+            decimalClicked = true;
+        }
+
+        if (['*', '+', '/', '-'].includes(e.key)) {
+            let currentVal = input.textContent.split(' ');
+
+            if (input.textContent == 'NaN' || input.textContent == 'Yeah no.') {
+                input.textContent = ` ${e.key} `;
+            } else if (['+', '-', '*', '/'].includes(currentVal.at(-2)) && !currentVal.at(-1)) {
+                currentVal.splice(-2, 1, `${e.key}`);
+                input.textContent = currentVal.join(' ');
+            } else {
+                input.textContent += ` ${e.key} `;
+            }
+            
+            decimalClicked = false;
+        }
+
+        if (e.key == 'Enter') {
+            const inputArr = input.textContent
+                .split(' ')
+                .map(item => (item || '0'));
+            let prevNum;
+            let nextNum;
+
+            if (!['+', '-', '*', '/'].includes(inputArr[0])) {
+                prevNum = inputArr[0];
+            } else {
+                prevNum = '0';
+            }
+
+            if (inputArr.length > 1) {
+                for (let i = 0; i <= inputArr.length - 1; i++) {
+                    if (['+', '-', '*', '/'].includes(inputArr[i])) {
+                        if (i == inputArr.length - 1) {
+                            nextNum = '0';
+                        } else {
+                            nextNum = inputArr[i + 1];
+                        }
+                        let newNum = operate(inputArr[i], prevNum, nextNum);
+                        prevNum = newNum;
+                        console.log(prevNum);
+                    }
+                }
+            }
+
+            input.textContent = prevNum;
+            answer.textContent = prevNum;
+
+            if (input.textContent.split('').includes('.')) {
+                decimalClicked = true;
+            } else {
+                decimalClicked = false;
+            }
+        }
+
+        if (e.key == 'Backspace') {
+            const currentVal = Array.from(input.textContent);
+
+            if (currentVal.at(-2) == '+' || currentVal.at(-2) == '-' || currentVal.at(-2) == '*' || currentVal.at(-2) == '/') {
+                input.textContent = currentVal
+                    .slice(0, -3)
+                    .join('');
+                
+                if (input.textContent.split(' ').at(-1).split('').includes('.')) {
+                    decimalClicked = true;
+                } else {
+                    decimalClicked = false;
+                }
+            } else {
+                if (currentVal.at(-1) == '.') {
+                    decimalClicked = false;
+                }
+                input.textContent = currentVal
+                    .slice(0, -1)
+                    .join('');
+            }
+        }
+
+        if (e.key == 'Escape') {
+            answer.textContent = '0';
+            input.textContent = null;
+            decimalClicked = false;
+        }
+    }
 })
